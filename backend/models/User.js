9a1fa03c98e32cf5c6602c1b60c6e0045d43e7d8
Mouhamed Ndiaye <mouhandiayeuh13@gmail.com>
2025-12-03 03:07:40 -0800

    l'inscription marche et la connexion aussi marche

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,12 +1,9 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  tel: { type: String, required: true },
-  adresse: { type: String, required: true },
   password: { type: String, required: true },
-  role: { type: String, default: "user" }
-}, { timestamps: true });
+});
 
-module.exports = mongoose.model("User", userSchema);
+export default mongoose.model("User", UserSchema);
