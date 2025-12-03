1dc2270aa067d18a6cb72b1ed1361def347d827e
sene32304@gmail.com <sene32304@gmail.com>
2025-12-03 10:19:59 +0000

    modificat accountController

--- backend/models/Account.js
+++ backend/models/Account.js
@@ -1,34 +1,86 @@
 
+// const mongoose = require("mongoose");
+
+// const accountSchema = new mongoose.Schema({
+//     user: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: "User",
+//         required: true
+//     },
+//     type: {
+//         type: String,
+//         enum: ["courant", "epargne", "business"],
+//         required: true
+//     },
+   
+//     accountNumber: {
+//         type: String,
+//         unique: true,
+//         required: true
+//     },
+//     balance: {
+//         type: Number,
+//         default: 0
+//     },
+// }, { timestamps: true });
+
+// // Générer un numéro de compte unique automatiquement
+// accountSchema.pre("save", async function(next) {
+//     if (!this.accountNumber) {
+//         this.accountNumber = "ACC-" + Math.floor(100000000 + Math.random() * 900000000);
+//     }
+//     next();
+// });
+
+// module.exports = mongoose.model("Account", accountSchema);
+
+
 const mongoose = require("mongoose");
 
-const accountSchema = new mongoose.Schema({
-    user: {
+const AccountSchema = new mongoose.Schema({
+    userId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
         required: true
     },
+
+    // Types de comptes 
     type: {
         type: String,
         enum: ["courant", "epargne", "business"],
         required: true
     },
-    accountNumber: {
+
+    // Nom affiché dans le dashboard
+    name: {
         type: String,
-        unique: true,
         required: true
     },
+
+    // IBAN pour virements externes
+    iban: {
+        type: String,
+        unique: true
+    },
+
+    currency: {
+        type: String,
+        default: "Fcfa"
+    },
+
     balance: {
         type: Number,
         default: 0
-    },
+    }
+
 }, { timestamps: true });
 
-// Générer un numéro de compte unique automatiquement
-accountSchema.pre("save", async function(next) {
-    if (!this.accountNumber) {
-        this.accountNumber = "ACC-" + Math.floor(100000000 + Math.random() * 900000000);
+// Génération automatique IBAN
+AccountSchema.pre("save", function(next) {
+    if (!this.iban) {
+        this.iban = "IBAN-" + Math.floor(1000000000000000 + Math.random() * 9000000000000000);
     }
     next();
 });
 
-module.exports = mongoose.model("Account", accountSchema);
+module.exports = mongoose.model("Account", AccountSchema);
