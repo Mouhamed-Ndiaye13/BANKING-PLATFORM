95197ad67aa508d7d7eb3d4d63e814ecb31dd543
Mouhamed Ndiaye <mouhandiayeuh13@gmail.com>
2025-12-04 04:57:41 -0800

    email envoyer avec success

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,5 +4,7 @@ const ctrl = require('../controllers/authController');
 
 router.post('/register', ctrl.register);
 router.post('/login', ctrl.login);
+router.post('/forgot-password', ctrl.forgotPassword);
+router.post('/reset-password/:token', ctrl.resetPassword);
 
 module.exports = router;
