bfab73d7d075c0276d2114c604f4f620526356de
sene32304@gmail.com <sene32304@gmail.com>
2025-12-04 18:02:20 +0000

    modifi auth

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,12 +2,15 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true },         
+  prenom: { type: String, required: true },      
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  dateDeNaissance: { type: Date, required: true }, 
   resetToken: String,
   resetTokenExpire: Date,
-});
+}, { timestamps: true }); 
 
 export default mongoose.model("User", UserSchema);
+
 
