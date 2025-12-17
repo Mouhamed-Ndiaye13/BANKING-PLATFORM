e787a5339c9f94631664a33c1b9250a1a88d4bda
sene32304@gmail.com <sene32304@gmail.com>
2025-12-17 09:07:49 +0000

    init

--- backend/models/Beneficiaire.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const beneficiaireSchema = new mongoose.Schema({
-  accountId: { type: String, required: true },
-  nom: { type: String, required: true },
-  type: { type: String, required: true },
-});
-
-export default mongoose.model("Beneficiaire", beneficiaireSchema);
