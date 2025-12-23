b1c920309094f91acd5d26d03b187cdd85ed1106
Nogaye Ndao <nogayendao2020@gmail.com>
2025-12-23 09:44:56 +0000

    connexion avec google

--- backend/routes/googleAuthRoutes.js
+++ backend/routes/googleAuthRoutes.js
@@ -1,9 +1,10 @@
+// routes/googleAuthRoutes.js
 import express from "express";
 import { googleLogin } from "../controllers/googleAuthController.js";
 
 const router = express.Router();
 
-// POST /auth/google
-router.post("/", googleLogin);
+// POST /api/auth/google
+router.post("/google", googleLogin);
 
 export default router;
