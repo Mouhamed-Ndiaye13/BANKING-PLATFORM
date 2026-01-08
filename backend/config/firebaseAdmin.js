2fd2a14003d05586a20dd7627d92e2a5304ea7c7
Mouhamed Ndiaye <mouhandiayeuh13@gmail.com>
2026-01-07 15:22:10 -1200

    deploy 27

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,21 +1,14 @@
 import admin from "firebase-admin";
-import dotenv from "dotenv";
 
-dotenv.config();
+// Remplace les \n par de vrais retours à la ligne
+const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n");
 
-// Vérification des variables d'environnement
-if (
-  !process.env.FIREBASE_PROJECT_ID ||
-  !process.env.FIREBASE_CLIENT_EMAIL ||
-  !process.env.FIREBASE_PRIVATE_KEY
-) {
+// Vérification des variables
+if (!process.env.FIREBASE_PROJECT_ID || !process.env.FIREBASE_CLIENT_EMAIL || !privateKey) {
   throw new Error("Les variables d'environnement Firebase ne sont pas définies !");
 }
 
-// Remplacer les \n par de vrais retours à la ligne
-const privateKey = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n");
-
-// Initialisation sécurisée de Firebase Admin
+// Initialisation sécurisée
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
@@ -26,4 +19,6 @@ if (!admin.apps.length) {
   });
 }
 
+export const auth = admin.auth();
+export const db = admin.firestore();
 export default admin;
