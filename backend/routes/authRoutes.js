18a0c43b404ccff8c92f1d4e885caed83d6f7b8d
Mouhamed Ndiaye <mouhandiayeuh13@gmail.com>
2026-01-08 13:32:03 -1200

    affichage dans frontend 44 risque

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,41 +1,49 @@
-import express from "express"
+import express from "express";
 import {
-  register,confirmEmail,
+  register,
   login,
+  checkEmail,
+  confirmEmail,
   verifyEmail2FA,
-  toggleTwoFA ,
+  toggleTwoFA,
   forgotPassword,
-  resetPassword
+  resetPassword,
+  loginWithGoogle
 } from "../controllers/authController.js";
 import Auth from "../middleware/auth.js";
-
 
 const router = express.Router();
 
 // Inscription
 router.post("/register", register);
 
-// Vérifier email avant inscription
+// Vérifier si email existe (frontend)
 router.get("/check-email", checkEmail);
 
-router.get("/confirm-email/:token", confirmEmail);
-// Login (peut déclencher 2FA)
+// Login classique
 router.post("/login", login);
+
+// Login Google
+router.post("/login/google", loginWithGoogle);
 
 // Vérification code 2FA email
 router.post("/verify-email-2fa", verifyEmail2FA);
 
+// Activer/désactiver 2FA
 router.put("/settings/two-factor", Auth, toggleTwoFA);
+
 // Mot de passe oublié
 router.post("/forgot-password", forgotPassword);
 
 // Réinitialisation mot de passe
 router.post("/reset-password/:token", resetPassword);
 
-// Exemple route protégée (nécessite JWT)
+// Confirmer email
+router.get("/confirm-email/:token", confirmEmail);
+
+// Exemple route protégée
 router.get("/protected", Auth, (req, res) => {
   res.json({ message: `Hello ${req.user.id}, tu es authentifié !` });
 });
-
 
 export default router;
